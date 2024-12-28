0e7a1c20b504680638876bb3288e54452ccd31a4
CAILeiz <1912956608@qq.com>
2024-12-28 18:02:34 +0800

    update: support: div > span(text)

--- src/index.js
+++ src/index.js
@@ -10,12 +10,42 @@ export function getFirstSelection(window) {
         startContainer,
         endContainer,
         collapsed,
+        commonAncestorContainer,
       } = range;
+      res.collapsed = collapsed;
       let str;
       if (endContainer === startContainer) {
         str = startContainer.textContent.slice(startOffset, endOffset);
       } else {
-        str = startContainer.textContent.slice(startOffset);
+        const startTextParentEl = startContainer.parentElement.parentElement;
+        if (startTextParentEl) {
+          let lastDoms = Array.from(startTextParentEl.children);
+          const startIdx = lastDoms.findIndex(
+            (item) => item === startContainer.parentElement
+          );
+          let endIndex;
+          if (startTextParentEl === commonAncestorContainer) {
+            endIndex = lastDoms.findIndex(
+              (item) => item === endContainer.parentElement
+            );
+            lastDoms = lastDoms.slice(startIdx, endIndex + 1);
+          } else {
+            lastDoms = lastDoms.slice(startIdx);
+          }
+          console.log(endIndex);
+
+          str = lastDoms
+            .reduce((text, dom, curIdx) => {
+              const curText =
+                endIndex && curIdx === lastDoms.length - 1
+                  ? dom.textContent.slice(0, endOffset)
+                  : dom.textContent;
+              return text + curText;
+            }, "")
+            .slice(startOffset);
+        } else {
+          throw "处理父子节点失败";
+        }
       }
       // console.log("当前第一个容器已选文本", str);
       res.success = true;
